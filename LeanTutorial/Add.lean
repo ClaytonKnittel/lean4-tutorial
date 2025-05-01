5891198f08989b447597d697cd9f8777a63cc14f
Clayton Knittel <cknit1999@gmail.com>
2025-04-30 23:41:04 -0700

    Mul world finished

--- LeanTutorial/Add.lean
+++ LeanTutorial/Add.lean
@@ -1,20 +1,20 @@
 import Mathlib.Tactic.NthRewrite
 import LeanTutorial.MyNat
 
-def myAdd : ℕ → ℕ → ℕ
+def addNat : ℕ → ℕ → ℕ
   | .zero, n => n
-  | .succ m, n => MyNat.succ (myAdd m n)
+  | .succ m, n => MyNat.succ (addNat m n)
 
-infixl:65 " + " => myAdd
+infixl:65 " + " => addNat
 
 theorem succ_add (a b : ℕ) : a.succ + b = (a + b).succ :=
   by
-    rw [myAdd]
+    rw [addNat]
 
 theorem add_succ (a b : ℕ) : a + b.succ = (a + b).succ :=
   by
     induction a with
-    | zero => repeat rw [myAdd]
+    | zero => repeat rw [addNat]
     | succ _ ih =>
       repeat rw [succ_add]
       rw [ih]
@@ -22,8 +22,8 @@ theorem add_succ (a b : ℕ) : a + b.succ = (a + b).succ :=
 theorem add_zero (a : ℕ) : a + MyNat.zero = a :=
   by
     induction a with
-    | zero => rw [myAdd]
-    | succ _ ih => rw [myAdd, ih]
+    | zero => rw [addNat]
+    | succ _ ih => rw [addNat, ih]
 
 theorem zero_add (a : ℕ) : MyNat.zero + a = a :=
   by
