8416151c6bee336f77d32cbafd7cea8239eb9821
Clayton Knittel <cknit1999@gmail.com>
2025-04-30 21:38:01 -0700

    Addition world

--- LeanTutorial/MyNat.lean
+++ LeanTutorial/MyNat.lean
@@ -1,3 +1,5 @@
+import Mathlib.Tactic.NthRewrite
+
 inductive MyNat where
 | zero : MyNat
 | succ : MyNat -> MyNat
@@ -26,21 +28,41 @@ theorem succ_add (a b : ℕ) : a.succ + b = (a + b).succ :=
   by
     rw [myAdd]
 
+theorem add_succ (a b : ℕ) : a + b.succ = (a + b).succ :=
+  by
+    induction a with
+    | zero => repeat rw [myAdd]
+    | succ _ ih =>
+      repeat rw [succ_add]
+      rw [ih]
+
 theorem add_zero (a : ℕ) : a + MyNat.zero = a :=
   by
     induction a with
-    | zero =>
-      rewrite [myAdd]
-      eq_refl
-    | succ b ih =>
-      rewrite [myAdd]
-      rewrite [ih]
-      eq_refl
+    | zero => rw [myAdd]
+    | succ _ ih => rw [myAdd, ih]
 
-theorem add_succ (a b : ℕ) : a + b.succ = (a + b).succ :=
+theorem zero_add (a : ℕ) : MyNat.zero + a = a :=
   by
-    induction b with
-    | zero =>
-      rewrite [add_zero]
-      sorry
-    | succ => sorry
+    induction a with
+    | zero => rw [add_zero]
+    | succ b ih => rw [add_succ, ih]
+
+theorem add_comm (a b : ℕ) : a + b = b + a :=
+  by
+    induction a with
+    | zero => rw [add_zero, zero_add]
+    | succ _ ih => rw [add_succ, succ_add, ih]
+
+theorem add_assoc (a b c : ℕ) : (a + b) + c = a + (b + c) :=
+  by
+    induction a with
+    | zero => repeat rw [zero_add]
+    | succ _ ih =>
+      repeat rw [succ_add]
+      rw [ih]
+
+theorem add_right_comm (a b c : ℕ) : (a + b) + c = (a + c) + b :=
+  by
+    rw [add_comm, ← add_assoc]
+    nth_rw 2 [add_comm]
