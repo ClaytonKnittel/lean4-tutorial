b3ce64844d5a4dd5c275694b3e27a7fcd55bd1aa
Clayton Knittel <cknit1999@gmail.com>
2025-05-04 17:11:08 -0700

    Set

---
+++ LeanTutorial/Set.lean
@@ -0,0 +1,18 @@
+
+universe u
+
+def MySet (α : Type u) := α → Prop
+
+namespace MySet
+
+def Mem (s : MySet α) (a : α) : Prop :=
+  s a
+
+instance : Membership α (MySet α) :=
+  ⟨MySet.Mem⟩
+
+def Subset (s₁ s₂ : MySet α) :=
+  ∀ a, a ∈ s₁ → a ∈ s₂
+
+instance : HasSubset (MySet α) :=
+  ⟨MySet.Subset⟩
